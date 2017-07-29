682833aa35262cb509f9d364d86649a1dba233c9
sauber92 <jjy920517@gmail.com>
2017-07-29 14:56:13 +0900

    부트스트랩 & Onsen UI 추가

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <p class="msg">{{ msg }}</p>
+
+    <form class="login-form">
+      <div class="form-group row">
+        <div class="col">
+          <input class="form-control" type="email" value="이메일을 입력하세요" id="example-text-input">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col">
+          <input class="form-control" type="password" value="비밀번호를 입력하세요" id="example-search-input">
+        </div>
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: '프로귀찮러를 위한 소비관리'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .msg {
+    font-size: 22px;
+    font-weight: 500;
+    color: #787878;
+  }
+
+  form {
+    /*text-align: center;*/
+  }
+
+</style>
