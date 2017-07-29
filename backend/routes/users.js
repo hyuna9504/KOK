01e0c887d90afdf581066924237188a0d26991c2
hyuna <hyuna9509@naver.com>
2017-07-29 20:38:10 +0900

    naver profile 조회

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -30,25 +30,45 @@ router.get('/auth', function(req, res, next) {
     url: api_url,
     headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
   };
+
   request.get(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log("yeah");
+      req.session.token = body.access_token;
+      console.log(body.access_token);
       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
       res.end(body);
-      // admin.database().ref('users').set({
-      //     username: "hi"
-      // }).then((id) => {
-      //     res.status(200).json("hi");
-      // })
-
-      //sess.userId = ""
-
     } else {
       res.status(response.statusCode).end();
       console.log('error = ' + response.statusCode);
     }
   });
 });
+
+router.get('/member', function (req, res) {
+   var token = req.session.token;
+   console.log(token);
+   var header = "Bearer " + token;
+   var api_url = 'https://openapi.naver.com/v1/nid/me';
+   var request = require('request');
+   var options = {
+       url: api_url,
+       headers: {'Authorization': header}
+    };
+   request.get(options, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+       res.end(body);
+       console.log(body);
+     } else {
+       console.log('error');
+       if(response != null) {
+         res.status(response.statusCode).end();
+         console.log('error = ' + response.statusCode);
+       }
+     }
+   });
+ });
+
 
 router.get('/add', function(req, res, next) {
   const newRef = admin.database().ref('users').push();
